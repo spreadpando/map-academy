0f25a5b4666793f64d786e8f7b828b39d44fde36
pando <spreadpando@gmail.com>
2020-10-09 16:44:32 -0600

    teammate caption update

--- src/Team.js
+++ src/Team.js
@@ -19,13 +19,13 @@ const Team = () => {
     <>
       <TeamHeader id='team'>Team</TeamHeader>
       <TeamContainer>
-        <TeamMember name='Mimi Ward MA, CALP' title='Founder & CEO' img='/img/team/mimi-avatar.jpg' />
-        <TeamMember name='Brad Fanestil MD' title='Mind/Body Specialist' img='/img/team/brad-avatar.jpg' />
-        <TeamMember name='Cathy Clark MA' title='Executive Coach Boulder Ed. Strategies' img='/img/team/cathy-avatar.jpg' />
         <TeamMember
-          name='Eleni Arapkiles' title='
-          Literature and writing specialist
-          32 yr Bvsd English teacher' img='/img/team/leni-avatar.jpg'
+          name='Mimi Ward MA, CALP' title={'Dyslexia Therapist, Teacher\nBoulder Dyslexia Therapy'} img='/img/team/mimi-avatar.jpg'
+        />
+        <TeamMember name='Brad Fanestil MD' title={'Mind Body Specialist\nBCH Center for Mind Body Medicine'} img='/img/team/brad-avatar.jpg' />
+        <TeamMember name='Cathy Clark MA' title={'Executive Skills Coach\nBolderED Strategies LLC'} img='/img/team/cathy-avatar.jpg' />
+        <TeamMember
+          name='Eleni Arapkiles' title={'Literature and Writing Specialist \n 32 yr BVSD Teacher'} img='/img/team/leni-avatar.jpg'
         />
       </TeamContainer>
     </>
--- src/TeamMember.js
+++ src/TeamMember.js
@@ -5,6 +5,13 @@ const TeamMemberContainer = styled('div')`
     height: 15rem;
     text-align: center;
     display: inline-block;
+    h5{
+      margin: 1rem 0 0 0;
+    }
+    p {
+      margin: 0;
+      font-size: 0.8rem;
+    }
 `
 
 const TeamMemberAvatar = styled('div')`
@@ -25,9 +32,16 @@ const TeamMember = ({ name, title, img }) => {
       <TeamMemberAvatar>
         <img src={img} />
       </TeamMemberAvatar>
-      <h5>{name}<br />
-        {title}
-      </h5>
+      <h5>{name}</h5>
+      <p>
+        {title.split('\n').map((item, key) => {
+          return (
+            <span key={key}>
+              {item} <br />
+            </span>
+          )
+        })}
+      </p>
     </TeamMemberContainer>
   )
 }
